2702ddb06e0cc51b862a1b7d7ff35b6facb198a5
Nancy Alter <nancy.alter@gmail.com>
2022-08-09 19:32:12 -0400

    add seo update images

--- src/App.js
+++ src/App.js
@@ -3,9 +3,23 @@ import Banner from './pages/Banner'
 import Charities from './pages/Charities'
 import Drawer from './pages/Drawer'
 import Sponsors from './pages/Sponsors'
+import { SuperSEO } from 'react-super-seo'
 
 function App() {
   return (<div>
+    <SuperSEO
+      title="Mercer County Turkey Trot"
+      description="Thanksgiving Morning 5k run and 1k walk. Raise money for local charities and food pantries.  Mercer County, West Windsor, NJ."
+      lang="en"
+      openGraph={{
+        ogImage: {
+          ogImage: './mcttlogo2022.png',
+          ogImageAlt: 'Mercer County Turkey Trot Logo',
+          ogImageWidth: 950,
+          ogImageHeight: 950,
+          ogImageType: 'image/png',
+        },
+      }} />
   <Drawer />
   <Banner />
   <About />
