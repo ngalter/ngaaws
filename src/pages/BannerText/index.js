b6396479923780c2dbf4a145b8b0ad9fa6b0d12b
Nancy Alter <nancy.alter@gmail.com>
2022-08-10 08:17:32 -0400

    spacing

--- src/pages/BannerText/index.js
+++ src/pages/BannerText/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import SignUpButton from '../SignUpButton';
-import styles from '../../styles/bannertext.css';
+import '../../styles/bannertext.css';
 
 function BannerText() {
   
-    return (<div className={styles.bannertextWrap} id={'bannertext'}>
+    return (<div className={"bannertextWrap"} id={'bannertext'}>
         <div className={"btTitle1"}>10th Annual Mercer County Turkey Trot</div>
         <div className={"btTitle2"}>Thursday, November 24, 2022</div>
         <div className={"btTitle3"}>Mercer County Park, West Windsor, NJ</div>
